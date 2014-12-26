d0f1c54354fb115da33687d7bb623aec2f6977c5
Josh Berlin <josh.berlin@mutualmobile.com>
2014-12-26 12:35:22 -0600

    Adding files

---
+++ WriteMore/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Mixpanel-OSX-Community
+#define COCOAPODS_POD_AVAILABLE_Mixpanel_OSX_Community
+#define COCOAPODS_VERSION_MAJOR_Mixpanel_OSX_Community 2
+#define COCOAPODS_VERSION_MINOR_Mixpanel_OSX_Community 1
+#define COCOAPODS_VERSION_PATCH_Mixpanel_OSX_Community 0
+
